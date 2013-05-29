ef0e1f76a047db3e23f1a30bf6a76fe935b40b80
Annabelle Richard <annabelle.e.richard@gmail.com>
2013-05-29 11:41:04 -0700

    Upgraded to jQuery 2.0.1. Upgraded to jQuery UI 1.10.3. Replaced local jQuery libraries with jQuery CDN.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-1.5.1.js
-//= require jquery-ui-1.8.10.custom.js
 //= require JSPlayer.js
 //= require JSPlayer.Event.js
 //= require_tree .
